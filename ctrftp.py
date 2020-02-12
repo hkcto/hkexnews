fc3c8351e32a760446f2a91efd4e16a9a32f36f6
hkcto <hksohoko@hotmail.com>
2020-02-12 16:00:37 +0800

    ftp

--- ctrftp.py
+++ ctrftp.py
@@ -1,4 +1,11 @@
 from ftplib import FTP
 
 
+host ='chianteck.com'
+user = 'chiantec'
+password = r'bLfwipr4CoE0EFpR a X'
+ftp = FTP(host, user, password)
+# ftp.login()
 
+ftp.cwd('www')
+ftp.retrlines('LIST')
