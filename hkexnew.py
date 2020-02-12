af8958562daea85d1b28f05280fe592958faa6c5
hksohoko <hksohoko@hotmail.com>
2020-02-12 20:33:37 +0800

    new

--- hkexnew.py
+++ hkexnew.py
@@ -4,6 +4,110 @@ from urllib import parse
 from lxml import etree
 import os
 
+log = []
+if os.path.isfile('savelog.txt'):
+  with open('savelog.txt', 'r') as f:
+    for line in f:
+      log.append(line.strip())
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"}
+
+
+def spider(url, payload, headers=header):
+    # post 請求
+    r = requests.post(url, payload, headers=header)
+    html = r.text
+    html = etree.HTML(html) # 生成一個 xpath 解釋對像
+
+    # 發表時間
+    release_time = html.xpath('//td[@class="text-right release-time"]/text()')
+    release_time= change_list(release_time) # 這行係為了刪除空白或無用的字符的list元素
+    # print("release time: \n", release_time)
+
+    #   table headline 文件標題
+    doc_headline = html.xpath('//div[@class="headline"]/text()')
+    doc_headline = change_list(doc_headline, s= 0)
+    # print('doc_headline', doc_headline)
+
+    # document content 文件說明.
+    doc_content = html.xpath('//div[@class="doc-link"]/a/text()')
+    doc_content = change_list(doc_content,s=0)
+    # print("doc_content\n",doc_content)
+
+    # a href a 文件超連結
+    doc_link = html.xpath('//div[@class="doc-link"]/a/@href')
+    doc_link_new = html.xpath('//div[@class="doc-link"]/a/@href')
+    # print("doc_link\n",doc_link)
+    hkexnews_link = 'https://www1.hkexnews.hk'
+
+    # 判斷 a href 是什麽類型 pdf or htm. pdf 就要下載, htm　就需進一步爬取和分析
+    # pdf_uri 用來保存所有需要下載的 pdf 檔案
+    # htm_uri 用來保存需要再次分析的 url
+    pdf_uri = []
+    htm_uri = []
+    i = 0
+    for u in doc_link:
+        filetype = os.path.splitext(u)[1]  # os.path.splitext() 可以分離出檔案的名和後綴
+    if filetype == '.pdf':
+        link = hkexnews_link + u
+        pdf_uri.append(link)
+        #print("pdf_uri 添加了: ", link)
+    elif filetype == '.htm':
+        link = hkexnews_link + u
+        htm_uri.append(link)
+        #print("htm_uri 添加了:", link)
+        pdf_uri.extend(gethtmpdf(link))
+        # 在這裡直接下載 htm 文件,有別於pdf集中下戴
+        if link not in log:
+            download(link)
+            with open('savelog.txt', 'a') as f:
+                f.write('\n' + link)
+        else:
+            print('己存在:  ', link)
+    else:
+        pass
+    i = i+1
+
+
+    #   將 a href 和 hkexnews link 合併,得到完整的 pdf URI,有了完整的URL才能下載其PDF檔
+    # i = 0
+    # for s in doc_link:
+    #   doc_link[i] = hkexnews_link + s
+    #   i = i+1
+
+
+    # 執行下載文件
+    i = 0
+    for u in pdf_uri:
+        if u not in log:
+            download(pdf_uri[i])
+            with open('savelog.txt', 'a') as f:
+                f.write('\n' + u)
+                i = i+1
+        else:
+            print('己存在:  ', u)
+
+
+    # 用於生成 html code
+    str = """<tr><td class="td-date">{date}</td><td><div class="headline">{document}</div><div class="doc-link"><a href={doc_link} style="text-decoration:none;">{doc_link_content}</a></div></td></tr>\n"""
+    tablecode = ""
+    i = 0
+    for s in release_time:
+        # print(str.format(date=s))
+        tablecode = tablecode + str.format(date=s, document=doc_headline[i], doc_link=doc_link_new[i], doc_link_content=doc_content[i])
+        i = i+1
+
+
+
+    tablecode = '<div style="width:15%;display:inline-block;"></div><div id="table-div" style="display:inline-block"><table><thead><th style="border:0;">Date</th><th style="border:0;">Document</th></thead>' + tablecode + '</table></div>'
+    headercode = """<section id="banner"></div><div style="width:10%;display:inline-block"></div><h2 style="display:inline-block">Announcements and Circulars</h2></section>\n"""
+    tablecode = headercode + tablecode
+
+    return tablecode
+
+
+
+
 # change_list def
 def change_list(xpath_list, s = 1):
     xpath_list = [str(x) for x in xpath_list] # 在這裡將所有 list 中的元素轉為 str 型態
--- run.py
+++ run.py
@@ -3,21 +3,12 @@ import requests
 import os
 from lxml import etree
 
-
-# 讀取 log 
-log = []
-if os.path.isfile('savelog.txt'):
-  with open('savelog.txt', 'r') as f:
-    for line in f:
-      log.append(line.strip())
-
 url = ['https://www1.hkexnews.hk/search/titlesearch.xhtml?lang=en',
       'https://www1.hkexnews.hk/search/titlesearch.xhtml?lang=zh']
 # url= 'https://www1.hkexnews.hk/search/titlesearch.xhtml?lang=en'
 # url_hk= 'https://www1.hkexnews.hk/search/titlesearch.xhtml?lang=zh'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"}
-
-payload = {
+payload_eng = {
     'lang':'en',
     'category': '0',
     'market': 'SEHK',
@@ -38,102 +29,35 @@ payload_hk = {
 
 }
 
-# post 請求
-r = requests.post(url[1], payload_hk, headers=headers)
-html = r.text
-html = etree.HTML(html) # 生成一個 xpath 解釋對像
+####################### 這裡是正式運行區 #################################
 
-#   table headline
-document = html.xpath('//div[@class="headline"]/text()')
-headline = hkexnew.change_list(document, s= 0)
-# print(headline)
+# 這裡是英文版
+tablecode = hkexnew.spider(url[0], payload_eng)
+wpxmlrpc.eidtpage(763, 'Announcements and Circulars', tablecode)
 
+# 這裡是中文版
+tablecode = hkexnew.spider(url[1], payload_hk)
+wpxmlrpc.eidtpage(36, '公告及通函', tablecode)
 
-# 發表時間
-table_time = html.xpath('//td[@class="text-right release-time"]/text()')
-table_time_new = hkexnew.change_list(table_time)
-# print(table_time_new)
+# 這裡是上載區
+pyftp.ftpupload()
 
 
-# document content 文件說明.
-doc_content = html.xpath('//div[@class="doc-link"]/a/text()')
-doc_content_new = hkexnew.change_list(doc_content,s=0)
-# print(doc_content_new)
+########################## 這裡是測試區 ##################################
+# # 這裡是英文版
+# tablecode = hkexnew.spider(url[0], payload_eng)
+# wpxmlrpc.eidtpage(1554, 'rpc_eng', tablecode)
 
-# a href a 超連接
-doc_link = html.xpath('//div[@class="doc-link"]/a/@href')
-doc_link_new = html.xpath('//div[@class="doc-link"]/a/@href')
-hkexnews_link = 'https://www1.hkexnews.hk'
-
-
-# 判斷 a href 是什麽類型 pdf or htm. pdf 就要下載, htm　就需進一步爬取和分析
-# pdf_uri 用來保存所有需要下載的 pdf 檔案
-# htm_uri 用來保存需要再次分析的 url
-pdf_uri = []
-htm_uri = []
-i = 0
-for u in doc_link:
-  filetype = os.path.splitext(u)[1]  # os.path.splitext() 可以分離出檔案的名和後綴
-  if filetype == '.pdf':
-    link = hkexnews_link + u
-    pdf_uri.append(link)
-    #print("pdf_uri 添加了: ", link)
-  elif filetype == '.htm':
-    link = hkexnews_link + u
-    htm_uri.append(link)
-    #print("htm_uri 添加了:", link)
-    pdf_uri.extend(hkexnew.gethtmpdf(link))
-    # 在這裡直接下載 htm 文件,有別於pdf集中下戴
-    if link not in log:
-      hkexnew.download(link)
-      with open('savelog.txt', 'a') as f:
-        f.write('\n' + link)
-    else:
-      print('己存在:  ', link)
-  else:
-    pass
-  i = i+1
+# # 這裡是中文版
+# tablecode = hkexnew.spider(url[1], payload_hk)
+# wpxmlrpc.eidtpage(1559, 'rpc_hk', tablecode)
 
 
-#   將 a href 和 hkexnews link 合併,得到完整的 pdf URI,有了完整的URL才能下載其PDF檔
-# i = 0
-# for s in doc_link:
-#   doc_link[i] = hkexnews_link + s
-#   i = i+1
-
-
-  # 執行下載文件
-i = 0
-for u in pdf_uri:
-  if u not in log:
-    hkexnew.download(pdf_uri[i])
-    with open('savelog.txt', 'a') as f:
-      f.write('\n' + u)
-    i = i+1
-  else:
-    print('己存在:  ', u)
-
-
-# 用於生成 html code
-str = """<tr><td class="td-date">{date}</td><td><div class="headline">{document}</div><div class="doc-link"><a href={doc_link} style="text-decoration:none;">{doc_link_content}</a></div></td></tr>\n"""
-tablecode = ""
-i = 0
-for s in table_time_new:
-    # print(str.format(date=s))
-    tablecode = tablecode + str.format(date=s, document=headline[i], doc_link=doc_link_new[i], doc_link_content=doc_content_new[i])
-    i = i+1
-
-
-
-tablecode = '<div style="width:15%;display:inline-block;"></div><div id="table-div" style="display:inline-block"><table><thead><th style="border:0;">Date</th><th style="border:0;">Document</th></thead>' + tablecode + '</table></div>'
-headercode = """<section id="banner"></div><div style="width:10%;display:inline-block"></div><h2 style="display:inline-block">Announcements and Circulars</h2></section>\n"""
-tablecode = headercode + tablecode
-# #hkexnew.changecontent('announcements.html','a2.html','Content',tablecode )
 
 # 修改 wp page. id: 1559 是中文版的 1554 是英文版.
 # id: 763 Announcements and Circulars
 # id: 36 公告及通函
-wpxmlrpc.eidtpage(36, '公告及通函', tablecode)
-wpxmlrpc.eidtpage(763, 'Announcements and Circulars', tablecode)
-pyftp.ftpupload()
+# wpxmlrpc.eidtpage(36, '公告及通函', tablecode)
+# wpxmlrpc.eidtpage(763, 'Announcements and Circulars', tablecode)
+#pyftp.ftpupload()
 
